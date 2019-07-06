325158ccb7dc071f84a4b48269115cdbd3dfd3d2
CarlosEscat <carlosescat@gmail.com>
2019-07-06 16:40:28 +0200

    section 4 write display

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -10,17 +10,22 @@ let hero = {
     }
 }
 
-function rest(hero){
-    if (hero.health == 10){
+function rest(object = hero){
+    
+    if (object.health == 10){
         alert('Health is full')
     }
-    hero.health = 10
+    object.health = 10
+    console.log(object.health)
+    hero = object
     return hero
 }
 
-function pickUpItem(hero, weaponlike){
-    hero.inventory.push(weaponlike)
-    
+function pickUpItem(herolike = hero, weaponlike){
+    herolike.inventory.push(weaponlike)
+    hero = herolike
+    console.log(hero.inventory)
+    return hero
 }
 
 function equipWeapon(hero){
@@ -35,3 +40,24 @@ function dagger(){
     let newDagger = {'type':'dagger','damage':2}
     pickUpItem(hero, newDagger)
 }
+
+function displayStats(){
+    const displayHero = document.getElementById('displayHero')
+
+    const labelName = document.createElement('p')
+    const labelHealth = document.createElement('p')
+    const labelWeapon = document.createElement('p')
+    const labelDamage = document.createElement('p')
+
+    labelName.textContent = 'Name: ' + hero.name
+    labelHealth.textContent = 'Health: ' + hero.health
+    labelWeapon.textContent = 'Weapon type: ' + hero.weapon.type
+    labelDamage.textContent = 'Damage: ' + hero.weapon.damage
+
+    displayHero.appendChild(labelName)
+    displayHero.appendChild(labelHealth)
+    displayHero.appendChild(labelWeapon)
+    displayHero.appendChild(labelDamage)
+}
+
+displayStats()
