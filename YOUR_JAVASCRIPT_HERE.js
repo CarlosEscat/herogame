39f470a5e4e2248a1e0da35e24bf495c6808c7b4
CarlosEscat <carlosescat@gmail.com>
2019-07-06 10:26:53 +0200

    hero object

---
+++ YOUR_JAVASCRIPT_HERE.js
@@ -0,0 +1,13 @@
+// Write your JS here
+let hero = {
+    "name" : "Pepe", 
+    "heroic" : true,
+    "inventory" : [],
+    "health" : 10,
+    "weapon" : {
+        "type" : "gun",
+        "damage" : 2
+    }
+
+}
+console.log(hero)
